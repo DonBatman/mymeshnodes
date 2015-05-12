11b5b301d4c58fc53b30a3d2d56cdd88e705eb92
DonBatman <serfdon@gmail.com>
2015-05-12 10:53:02 -0700

    Updated support files

--- ores.lua
+++ ores.lua
@@ -1,55 +1,156 @@
-
-
-
---function mymeshnodes.register_all(material, descr, image, groups, citem)
-
 local slope_cbox = {
 	type = "fixed",
 	fixed = {
-		{-0.5,  -0.5,  -0.5, 0.5, -0.25, 0.5},
-		{-0.5, -0.25, -0.25, 0.5,     0, 0.5},
-		{-0.5,     0,     0, 0.5,  0.25, 0.5},
-		{-0.5,  0.25,  0.25, 0.5,   0.5, 0.5}
+			{-0.5, -0.5, -0.5, 0.5, -0.375, 0.5},
+			{-0.5, -0.5, -0.375, 0.5, -0.25, 0.5},
+			{-0.5, -0.5, -0.25, 0.5, -0.125, 0.5},
+			{-0.5, -0.5, -0.125, 0.5, 0, 0.5},
+			{-0.5, -0.5, 0, 0.5, 0.125, 0.5},
+			{-0.5, -0.5, 0.125, 0.5, 0.25, 0.5},
+			{-0.5, -0.5, 0.25, 0.5, 0.375, 0.5},
+			{-0.5, -0.5, 0.375, 0.5, 0.5, 0.5},
 	}
 }
 
 local slope_cbox_long = {
 	type = "fixed",
 	fixed = {
-		{-0.5, -0.5,   -1.5,  0.5, -0.375, 0.5},  --  NodeBox1
-		{-0.5, -0.375, -1.25, 0.5, -0.25,  0.5},  --  NodeBox2
-		{-0.5, -0.25,  -1,    0.5, -0.125, 0.5},  --  NodeBox3
-		{-0.5, -0.125, -0.75, 0.5,  0,     0.5},  --  NodeBox4
-		{-0.5,  0,     -0.5,  0.5,  0.125, 0.5},  --  NodeBox5
-		{-0.5,  0.125, -0.25, 0.5,  0.25,  0.5},  --  NodeBox6
-		{-0.5,  0.25,   0,    0.5,  0.375, 0.5},  --  NodeBox7
-		{-0.5,  0.375,  0.25, 0.5,  0.5,   0.5},  --  NodeBox8
+		{-0.5, -0.5,   -1.5,  0.5, -0.375, 0.5},
+		{-0.5, -0.375, -1.25, 0.5, -0.25,  0.5},
+		{-0.5, -0.25,  -1,    0.5, -0.125, 0.5},
+		{-0.5, -0.125, -0.75, 0.5,  0,     0.5},
+		{-0.5,  0,     -0.5,  0.5,  0.125, 0.5},
+		{-0.5,  0.125, -0.25, 0.5,  0.25,  0.5},
+		{-0.5,  0.25,   0,    0.5,  0.375, 0.5},
+		{-0.5,  0.375,  0.25, 0.5,  0.5,   0.5},
 	}
 }
 
 local icorner_cbox = {
 	type = "fixed",
 	fixed = {
-		{-0.5, -0.5, -0.5, 0.5, -0.25, 0.5}, -- NodeBox5
-		{-0.5, -0.5, -0.25, 0.5, 0, 0.5}, -- NodeBox6
-		{-0.5, -0.5, -0.5, 0.25, 0, 0.5}, -- NodeBox7
-		{-0.5, 0, -0.5, 0, 0.25, 0.5}, -- NodeBox8
-		{-0.5, 0, 0, 0.5, 0.25, 0.5}, -- NodeBox9
-		{-0.5, 0.25, 0.25, 0.5, 0.5, 0.5}, -- NodeBox10
-		{-0.5, 0.25, -0.5, -0.25, 0.5, 0.5}, -- NodeBox11
+			{-0.5, -0.5, -0.5, 0.5, -0.375, 0.5},
+			{-0.5, -0.5, -0.375, 0.5, -0.25, 0.5},
+			{-0.5, -0.5, -0.25, 0.5, -0.125, 0.5},
+			{-0.5, -0.5, -0.125, 0.5, 0, 0.5},
+			{-0.5, -0.5, 0, 0.5, 0.125, 0.5},
+			{-0.5, -0.5, 0.125, 0.5, 0.25, 0.5},
+			{-0.5, -0.5, 0.25, 0.5, 0.375, 0.5}, 
+			{-0.5, -0.5, 0.375, 0.5, 0.5, 0.5}, 
+			{-0.5, -0.5, -0.5, -0.375, 0.5, 0.5},
+			{-0.5, -0.5, -0.5, -0.25, 0.375, 0.5},
+			{-0.5, -0.5, -0.5, -0.125, 0.25, 0.5}, 
+			{-0.5, -0.5, -0.5, 0, 0.125, 0.5}, 
+			{-0.5, -0.5, -0.5, 0.125, 0, 0.5},
+			{-0.5, -0.5, -0.5, 0.25, -0.125, 0.5}, 
+			{-0.5, -0.5, -0.5, 0.375, -0.25, 0.5}, 
 	}
 }
-
+local icorner_cbox_long = {
+	type = "fixed",
+	fixed = {
+		{-0.5, -0.5, -1.5, -0.25, 0.5, 0.5},
+		{-0.5, -0.5, 0.25, 1.5, 0.5, 0.5},
+		{-0.5, -0.5, 0, 1.5, 0.375, 0.5},
+		{-0.5, -0.5, -1.5, 0, 0.375, 0.5},
+		{-0.5, -0.5, -1.5, 0.25, 0.25, 0.5},
+		{-0.5, -0.5, -1.5, 0.5, 0.125, 0.5},
+		{-0.5, -0.5, -1.5, 0.75, 0, 0.5},
+		{-0.5, -0.5, -1.5, 1, -0.125, 0.5},
+		{-0.5, -0.5, -1.5, 1.25, -0.25, 0.5},
+		{-0.5, -0.5, -1.5, 1.5, -0.375, 0.5},
+		{-0.5, -0.5, -0.25, 1.5, 0.25, 0.5},
+		{-0.5, -0.5, -0.5, 1.5, 0.125, 0.5}, 
+		{-0.5, -0.5, -0.75, 1.5, 0, 0.5},
+		{-0.5, -0.5, -1, 1.5, -0.125, 0.5},
+		{-0.5, -0.5, -1.25, 1.5, -0.25, 0.5},
+	}
+}
+local i45_cbox = {
+	type = "fixed",
+	fixed = {
+			{0.375, -0.5, -0.5, 0.5, 0.5, 0.5},
+			{-0.5, -0.5, 0.375, 0.5, 0.5, 0.5},
+			{-0.375, -0.5, 0.25, 0.5, 0.5, 0.5},
+			{0.25, -0.5, -0.375, 0.5, 0.5, 0.5},
+			{-0.25, -0.5, 0.125, 0.5, 0.5, 0.5},
+			{0.125, -0.5, -0.25, 0.5, 0.5, 0.5},
+			{0, -0.5, -0.125, 0.5, 0.5, 0.5},
+			{-0.125, -0.5, 0, 0.5, 0.5, 0.5},
+			{-0.5, -0.5, -0.5, 0.5, -0.375, 0.5},
+			{-0.5, -0.5, -0.375, 0.5, -0.25, 0.5},
+			{-0.5, -0.5, -0.25, 0.5, -0.125, 0.5},
+			{-0.5, -0.5, -0.125, 0.5, 0, 0.5},
+			{-0.5, -0.5, 0, 0.5, 0.125, 0.5},
+			{-0.5, -0.5, 0.125, 0.5, 0.25, 0.5},
+			{-0.5, -0.5, 0.25, 0.5, 0.375, 0.5}, 
+			{-0.375, -0.5, -0.5, 0.5, -0.25, 0.5},
+			{-0.25, -0.5, -0.5, 0.5, -0.125, 0.5},
+			{-0.125, -0.5, -0.5, 0.5, 0, 0.5}, 
+			{0, -0.5, -0.5, 0.5, 0.125, 0.5}, 
+			{0.125, -0.5, -0.5, 0.5, 0.25, 0.5},
+			{0.25, -0.5, -0.5, 0.5, 0.375, 0.5},
+	}
+}
+local o45_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.4375, -0.5, 0.375, 0.5, -0.375, 0.5}, 
+			{0.375, -0.5, -0.4375, 0.5, -0.375, 0.5}, 
+			{0.25, -0.5, -0.3125, 0.5, -0.375, 0.5}, 
+			{-0.3125, -0.5, 0.25, 0.5, -0.375, 0.5}, 
+			{0.125, -0.5, -0.1875, 0.5, -0.375, 0.5}, 
+			{-0.1875, -0.5, 0.125, 0.5, -0.375, 0.5}, 
+			{0, -0.5, 0, 0.5, -0.375, 0.5}, 
+			{0.375, -0.5, -0.3125, 0.5, -0.25, 0.5}, 
+			{-0.3125, -0.5, 0.375, 0.5, -0.25, 0.5}, 
+			{0.25, -0.5, -0.1875, 0.5, -0.25, 0.5}, 
+			{-0.1875, -0.5, 0.25, 0.5, -0.25, 0.5}, 
+			{0.125, -0.5, 0, 0.5, -0.25, 0.5}, 
+			{0, -0.5, 0.125, 0.5, -0.25, 0.5},
+			{0.375, -0.5, -0.1875, 0.5, -0.125, 0.5},
+			{-0.1875, -0.5, 0.375, 0.5, -0.125, 0.5},
+			{0.25, -0.5, 0, 0.5, -0.125, 0.5},
+			{0, -0.5, 0.25, 0.5, -0.125, 0.5},
+			{0.125, -0.5, 0.125, 0.5, -0.125, 0.5},
+			{0.375, -0.5, 0, 0.5, 0, 0.5},
+			{0, -0.5, 0.375, 0.5, 0, 0.5},
+			{0.25, -0.5, 0.125, 0.5, 0, 0.5},
+			{0.125, -0.5, 0.25, 0.5, 0, 0.5},
+			{0.375, -0.5, 0.375, 0.5, 0.4375, 0.5},
+			{0.375, -0.5, 0.25, 0.5, 0.3125, 0.5},
+			{0.375, -0.5, 0.125, 0.5, 0.1875, 0.5},
+			{0.25, -0.5, 0.375, 0.5, 0.3125, 0.5},
+			{0.125, -0.5, 0.375, 0.5, 0.1875, 0.5},
+			{0.25, -0.5, 0.25, 0.5, 0.1875, 0.5},
+	}
+}
 local ocorner_cbox = {
 	type = "fixed",
 	fixed = {
-		{-0.5,  -0.5,  -0.5,   0.5, -0.25, 0.5},
-		{-0.5, -0.25, -0.25,  0.25,     0, 0.5},
-		{-0.5,     0,     0,     0,  0.25, 0.5},
-		{-0.5,  0.25,  0.25, -0.25,   0.5, 0.5}
+			{-0.5, -0.5, -0.5, 0.5, -0.375, 0.5},
+			{-0.5, -0.5, -0.375, 0.375, -0.25, 0.5},
+			{-0.5, -0.5, -0.25, 0.25, -0.125, 0.5},
+			{-0.5, -0.5, -0.125, 0.125, 0, 0.5},
+			{-0.5, -0.5, 0, 0, 0.125, 0.5},
+			{-0.5, -0.5, 0.125, -0.125, 0.25, 0.5},
+			{-0.5, -0.5, 0.25, -0.25, 0.375, 0.5},
+			{-0.5, -0.5, 0.375, -0.375, 0.5, 0.5}, 
 	}
 }
-
+local ocorner_cbox_long = {
+	type = "fixed",
+	fixed = {
+		{-0.5, -0.5, 0.25, -0.25, 0.5, 0.5},
+		{-0.5, -0.5, 0, 0, 0.375, 0.5},
+		{-0.5, -0.5, -0.25, 0.25, 0.25, 0.5},
+		{-0.5, -0.5, -0.5, 0.5, 0.125, 0.5}, 
+		{-0.5, -0.5, -0.75, 0.75, 0, 0.5}, 
+		{-0.5, -0.5, -1, 1, -0.125, 0.5}, 
+		{-0.5, -0.5, -1.25, 1.25, -0.25, 0.5}, 
+		{-0.5, -0.5, -1.5, 1.5, -0.375, 0.5},
+	}
+}
 local smpyr_cbox = {
 	type = "fixed",
 	fixed = {
@@ -73,26 +174,81 @@ local pyr_cbox = {
 local slope_fronthalf_cbox = {
 	type = "fixed",
 	fixed = {
-		{-0.5, -0.5,   -0.5,  0.5, -0.375, 0.5},  --  NodeBox1
-		{-0.5, -0.375, -0.25, 0.5, -0.25,  0.5},  --  NodeBox2
-		{-0.5, -0.25,  0,    0.5, -0.125, 0.5},  --  NodeBox3
-		{-0.5, -0.125, 0.25, 0.5,  0,     0.5},  --  NodeBox4
+		{-0.5, -0.5,   -0.5,  0.5, -0.375, 0.5},
+		{-0.5, -0.375, -0.25, 0.5, -0.25,  0.5},
+		{-0.5, -0.25,  0,    0.5, -0.125, 0.5},
+		{-0.5, -0.125, 0.25, 0.5,  0,     0.5},
 	}
 }
 
 local slope_backhalf_cbox = {
 	type = "fixed",
 	fixed = {
-		{-0.5, -0.5,   -0.5,  0.5, 0.125, 0.5},  --  NodeBox1
-		{-0.5, 0.125, -0.25, 0.5, 0.25,  0.5},  --  NodeBox2
-		{-0.5, 0.25,  0,    0.5, 0.375, 0.5},  --  NodeBox3
-		{-0.5, 0.375, 0.25, 0.5,  0.5,     0.5},  --  NodeBox4
+		{-0.5, -0.5,   -0.5,  0.5, 0.125, 0.5},
+		{-0.5, 0.125, -0.25, 0.5, 0.25,  0.5},
+		{-0.5, 0.25,  0,    0.5, 0.375, 0.5},
+		{-0.5, 0.375, 0.25, 0.5,  0.5,     0.5},
 	}
 }
 local circle_cbox = {
 	type = "fixed",
 	fixed = {
 		{-0.5, -0.5, -0.5,  0.5, -0.25, 0.5}, 
+	}
+}
+local longslope_top_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.5, -0.5, 0.25, 0.5, 0.5, 0.5}, 
+			{-0.5, -0.5, 0, 0.5, 0.375, 0.25}, 
+			{-0.5, -0.5, -0.25, 0.5, 0.25, 0}, 
+			{-0.5, -0.5, -0.5, 0.5, 0.125, -0.25},  
+	}
+}
+local longslope_bottom_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.5, -0.5, 0.25, 0.5, 0, 0.5},
+			{-0.5, -0.5, 0, 0.5, -0.125, 0.25},
+			{-0.5, -0.5, -0.25, 0.5, -0.25, 0}, 
+			{-0.5, -0.5, -0.5, 0.5, -0.375, -0.25}, 
+	}
+}
+local pole_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.125, -0.5, -0.125, 0.125, 0.5, 0.125}, 
+	}
+}
+local smquapyramid_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.5, -0.5, 0.25, -0.25, 0, 0.5}, 
+			{-0.5, -0.5, 0, -0.25, -0.125, 0.25},
+			{-0.5, -0.5, -0.25, -0.25, -0.25, 0}, 
+			{-0.5, -0.5, -0.5, -0.25, -0.375, -0.25},
+			{-0.25, -0.5, 0.25, 0, -0.125, 0.5}, 
+			{0, -0.5, 0.25, 0.25, -0.25, 0.5}, 
+			{0.25, -0.5, 0.25, 0.5, -0.375, 0.5}, 
+			{-0.25, -0.5, 0, 0, -0.25, 0.25}, 
+			{-0.25, -0.5, -0.25, 0, -0.375, 0}, 
+			{0, -0.5, 0, 0.25, -0.375, 0.25}, 
+	}
+}
+local quapyramid_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.5, -0.5, 0.25, -0.25, 0.5, 0.5}, 
+			{-0.5, -0.5, 0, -0.25, 0.375, 0.25}, 
+			{-0.5, -0.5, -0.25, -0.25, 0.25, 0}, 
+			{-0.5, -0.5, -0.5, -0.25, 0.125, -0.25}, 
+			{-0.25, -0.5, 0.25, 0, 0.375, 0.5}, 
+			{0, -0.5, 0.25, 0.25, 0.25, 0.5}, 
+			{0.25, -0.5, 0.25, 0.5, 0.125, 0.5}, 
+			{-0.25, -0.5, 0, 0, 0.25, 0.25}, 
+			{-0.25, -0.5, -0.25, 0, 0.125, 0}, 
+			{0, -0.5, 0, 0.25, 0.125, 0.25}, 
+			{-0.25, -0.5, -0.5, 0.5, 0, 0.25}, 
 	}
 }
 
@@ -119,28 +275,31 @@ for i in ipairs(bat_slopes) do
 	local img = bat_slopes[i][4]
 
 local slopes = {   --shape , Description , mesh , colbox
-	{ "slope" ,        "Slope" ,                      	 "mymeshnodes_twelve-twelve.obj",       "slope_cbox"},
-	{ "slope_ic" ,     "Inside Corner Slope" ,        	 "mymeshnodes_twelve-twelve-ic.obj",    "icorner_cbox"},
-	{ "slope_oc" ,     "Outside Corner Slope" ,       	 "mymeshnodes_twelve-twelve-oc.obj",    "ocorner_cbox"},	
-	{ "lslope" ,       "Long Slope" ,                 	 "mymeshnodes_six-twelve.obj",     	"slope_cbox_long"},
-	{ "lslope_ic" ,    "Inside Corner Long Slope" ,   	 "mymeshnodes_six-twelve-ic.obj",    	"icorner_cbox_long"},
-	{ "lslope_oc" ,    "Outside Corner Long Slope" ,  	 "mymeshnodes_six-twelve-oc.obj",    	"ocorner_cbox_long"},
-	{ "lslope_top" ,   "Long Slope Top" ,  			 "mymeshnodes_lslope_top.obj",    	""},
-	{ "lslope_bot" ,   "Long Slope Bottom" ,  		 "mymeshnodes_lslope_bot.obj",    	""},
-	{ "smpyramid" ,    "Small Pyramid" ,  			 "mymeshnodes_smpyramid.obj",    	"smpyr_cbox"},	
-	{ "pyramid" ,      "Pyramid" ,   			 "mymeshnodes_pyramid.obj",    		"pyr_cbox"},	
-	{ "smquapyramid" , "Small Quarter Pyramid" ,		 "mymeshnodes_smquapyramid.obj",    	""},		
-	{ "quapyramid" ,   "Quarter Pyramid" ,			 "mymeshnodes_quapyramid.obj",    	""},	
-	{ "circle" , 	   "Circle" ,				 "mymeshnodes_circle.obj",    		"circle_cbox"},	
-	{ "pole" , 	   "Pole" ,				 "mymeshnodes_pole.obj",    		""},	
+	{ "slope" ,        "Slope" ,                      	 "mymeshnodes_twelve-twelve.obj",       slope_cbox},
+	{ "slope_ic" ,     "Inside Corner Slope" ,        	 "mymeshnodes_twelve-twelve-ic.obj",    icorner_cbox},
+	{ "slope_oc" ,     "Outside Corner Slope" ,       	 "mymeshnodes_twelve-twelve-oc.obj",    ocorner_cbox},	
+
+	{ "slope_i45" ,     "Inside 45 Slope" ,        	 	"mymeshnodes_diag1.obj",    i45_cbox},
+	{ "slope_o45" ,     "Outside 45 Slope" ,       		 "mymeshnodes_diag2.obj",    o45_cbox},
+
+	{ "lslope" ,       "Long Slope" ,                 	 "mymeshnodes_six-twelve.obj",     	slope_cbox_long},
+	{ "lslope_ic" ,    "Inside Corner Long Slope" ,   	 "mymeshnodes_six-twelve-ic.obj",    	icorner_cbox_long},
+	{ "lslope_oc" ,    "Outside Corner Long Slope" ,  	 "mymeshnodes_six-twelve-oc.obj",    	ocorner_cbox_long},
+	{ "lslope_top" ,   "Long Slope Top" ,  			 "mymeshnodes_lslope_top.obj",    	longslope_top_cbox},
+	{ "lslope_bot" ,   "Long Slope Bottom" ,  		 "mymeshnodes_lslope_bot.obj",    	longslope_bottom_cbox},
+	{ "smpyramid" ,    "Small Pyramid" ,  			 "mymeshnodes_smpyramid.obj",    	smpyr_cbox},	
+	{ "pyramid" ,      "Pyramid" ,   			 "mymeshnodes_pyramid.obj",    		pyr_cbox},	
+	{ "smquapyramid" , "Small Quarter Pyramid" ,		 "mymeshnodes_smquapyramid.obj",    	smquapyramid_cbox},		
+	{ "quapyramid" ,   "Quarter Pyramid" ,			 "mymeshnodes_quapyramid.obj",    	quapyramid_cbox},	
+	{ "circle" , 	   "Circle" ,				 "mymeshnodes_circle.obj",    		circle_cbox},	
+	{ "pole" , 	   "Pole" ,				 "mymeshnodes_pole.obj",    		pole_cbox},	
 	{ "ichalf" , 	   "Inside Corner Half" ,		 "mymeshnodes_ichalf.obj",    		""},
 	{ "cylinder" ,     "Cylinder" ,   			 "mymeshnodes_cylinder.obj",    	""},	
 	{ "sphere" ,       "Sphere" ,     			 "mymeshnodes_sphere.obj",    		""},	
 	{ "rounded" ,      "Rounded" ,  			 "mymeshnodes_rounded.obj",    		""},	
 	{ "roundedc" ,     "Rounded Corner" ,  			 "mymeshnodes_rounded_corner.obj",    	""},
 	{ "cone" ,         "Cone" ,  				 "mymeshnodes_cone.obj",    		""},	
-	{ "blob" ,         "Blob" ,  				 "mymeshnodes_blob.obj",    		""},	
-	{ "barrel" ,       "Barrel" ,  			 	 "mymeshnodes_barrel.obj",    		""},
+	{ "blob" ,         "Blob" ,  				 "mymeshnodes_blob.obj",    		""},
 }
 
 for i in ipairs(slopes) do
@@ -164,9 +323,62 @@ minetest.register_node("mymeshnodes:"..shap.."_"..mat, {
 	selection_box = colbox,
 })
 
-end
-end
 
+
+local shape1_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.5, -0.5, -0.25, 0.5, 0, 0.25},
+		}
+	}
+local shape2_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.5, -0.5, -0.25, 0.5, 0, 0.25},
+			{-0.25, -0.5, -0.5, 0.25, 0, 0.5}
+		}
+	}
+local shape3_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.5, -0.5, -0.25, 0.5, 0, 0.25},
+			{-0.25, -0.5, -0.5, 0.25, 0, 0.25}
+		}
+	}
+local shape4_cbox = {
+	type = "fixed",
+	fixed = {
+			{-0.5, -0.5, -0.25, 0.25, 0, 0.25},
+			{-0.25, -0.5, -0.5, 0.25, 0, 0.25}
+		}
+	}
+local halfshapes = {
+{"shape1","Shape 1", shape1_cbox},
+{"shape2","Shape 2", shape2_cbox},
+{"shape3","Shape 3", shape3_cbox},
+{"shape4","Shape 4", shape4_cbox},
+}
+for i in ipairs (halfshapes) do
+local shapename = halfshapes[i][1]
+local descn = halfshapes[i][2]
+local nbox = halfshapes[i][3]
+
+minetest.register_node("mymeshnodes:"..shapename.."_"..mat, {
+	description = desc.." "..descn,
+	drawtype = "nodebox",
+	tiles = {image},
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {choppy=2, oddly_breakable_by_hand=2, not_in_creative_inventory=1},
+	sounds = default.node_sound_wood_defaults(),
+	on_place = minetest.rotate_node,
+	node_box = nbox,
+	collision_box = nbox,
+	selection_box = nbox,
+})
+end
+end
+end
 
 
 
